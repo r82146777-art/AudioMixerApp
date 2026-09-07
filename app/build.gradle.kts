a3b399fef4b8d2324a5801b9e994503ed8becd61
r82146777-art <r82146777@gmail.com>
2026-09-07 14:42:56 +0330

    Bump 1.0.65 fast mix + FX fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.audiomixer.app"
         minSdk = 24
         targetSdk = 34
-        versionCode = 61
-        versionName = "1.0.61"
+        versionCode = 65
+        versionName = "1.0.65"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
