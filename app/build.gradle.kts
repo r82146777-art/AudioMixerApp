cc4000ad0e40e7e811b4791940672460a29c57f8
r82146777-art <r82146777@gmail.com>
2026-09-07 15:48:36 +0330

    Bump 1.0.68 3-step bulletproof mix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.audiomixer.app"
         minSdk = 24
         targetSdk = 34
-        versionCode = 67
-        versionName = "1.0.67"
+        versionCode = 68
+        versionName = "1.0.68"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
